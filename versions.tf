133861e059ae736bff522d00fe8997ac7f9462f2
jamesmaddox-cf <98417364+jamesmaddox-cf@users.noreply.github.com>
2023-08-03 11:00:48 -0400

    Updated versions

--- versions.tf
+++ versions.tf
@@ -1,20 +1,10 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = "~> 1.5.0"
   required_providers {
 
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.42, < 5.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 2.1"
+      version = ">= 4.70, < 5.0"
     }
   }
-
-  provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-cloud-storage/v4.0.0"
-  }
-
 }
